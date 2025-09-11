3df9239d0b515dbf085dbc18e5e44c829f9ca1cd
DRosvel <david.rosvel97@gmail.com>
2025-09-11 13:43:32 -0600

    arreglo carrusel productos

--- app.js
+++ app.js
@@ -142,3 +142,19 @@ modal.addEventListener('click', (e) => {
 document.querySelectorAll('[data-close-modal]').forEach(el =>
   el.addEventListener('click', closeModal)
 );
+
+
+// Scroll animado a secciones
+document.querySelectorAll('a.scroll-to').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    const target = document.getElementById(targetId);
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
